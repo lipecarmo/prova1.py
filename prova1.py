320a99650d85990889a14326c0e77395605b374b
lipecarmo <lipeoliveira523@gmail.com>
2024-10-19 19:19:52 -0300

    Update prova1.py

---
+++ prova1.py
@@ -0,0 +1,11 @@
+# Criando uma lista de frutas
+
+lista_frutas = ["maçã", "banana", "laranja", "uva", "kiwi", "abacaxi"]
+
+# Convertendo a lista em uma tupla
+
+tupla_frutas = tuple(lista_frutas)
+
+# Exibindo o conteúdo da tupla
+
+print(tupla_frutas)
